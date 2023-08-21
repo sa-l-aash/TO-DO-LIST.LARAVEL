b2240923d32b41c1e5216f579ef6a2766e4617d5
sa-l-aash <salaashkaelo@gmail.com>
2023-08-21 13:18:39 +0300

    added user and task controller routes

--- toDoListLaravel/routes/api.php
+++ toDoListLaravel/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//task controller routes but with relation to the user model
+Route::get('/getUsers',[TaskController::class,'getUsers']);
+Route::get('/getUser',[TaskController::class,'getUser']);
+//user controller routes 
+Route::post('/createUser',[UserController::class,'createUser']);
+Route::get('/readAllUsers',[UserController::class,'readAllUsers']);
+Route::get('/readOneUser',[UserController::class,'readOneUser']);
+//task controller routes
+Route::post('/createTask',[TaskController::class,'createTask']);
+Route::get('/readOneTask',[TaskController::class,'readOneTask']);
+Route::get('/readAllTasks',[TaskController::class,'readAllTasks']);
+
+
